e1dcc795f439f9ccfe0121a95cd11cd9b5dc2aed
tfreitasf <tfreitasf@gmail.com>
2022-08-02 20:08:50 -0300

    created layout activity_add_business_card

--- app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/me/dio/businesscard/ui/MainActivity.kt
@@ -1,16 +1,25 @@
 package me.dio.businesscard.ui
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import me.dio.businesscard.R
+import androidx.appcompat.app.AppCompatActivity
 import me.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        insertListener()
+    }
+
+    private fun insertListener(){
+        binding.fabAddCard.setOnClickListener {
+            val intent = Intent (this@MainActivity, AddBusinessCardActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
